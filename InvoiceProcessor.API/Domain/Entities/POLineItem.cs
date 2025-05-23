12a679ad1598c07a5f5c931f1bed8b6ef02d4429
datpham0412 <tiendat041202@gmail.com>
2025-05-24 01:08:53 +1000

    add Purchase Order Line Item entity

---
+++ InvoiceProcessor.API/Domain/Entities/POLineItem.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Domain.Entities
+{
+    public class POLineItem
+    {
+        public Guid Id { get; set; }
+        public Guid PurchaseOrderId { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        public decimal Amount => Quantity * UnitPrice;
+
+        public virtual PurchaseOrder PurchaseOrder { get; set; } = new();
+    }
+}
