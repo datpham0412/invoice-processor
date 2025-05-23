61c315e36266e6ff708673153aaee1c13f943f21
datpham0412 <tiendat041202@gmail.com>
2025-05-24 01:09:05 +1000

    add Purchase Order entity

---
+++ InvoiceProcessor.API/Domain/Entities/PurchaseOrder.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Domain.Entities
+{
+    public class PurchaseOrder
+    {
+        public Guid Id { get; set; }
+
+        public string PoNumber { get; set; } = string.Empty;
+        public string VendorName { get; set; } = string.Empty;
+        public DateTime IssueDate { get; set; }
+
+        public List<POLineItem> LineItems { get; set; } = new();
+
+        public decimal TotalAmount => LineItems.Sum(item => item.Amount);
+    }
+}
