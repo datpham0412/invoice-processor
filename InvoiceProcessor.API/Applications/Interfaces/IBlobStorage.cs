baa2412a621eef88377985bc7fd3a0ea4b06c419
datpham0412 <tiendat041202@gmail.com>
2025-05-30 01:02:28 +1000

    feat: defince IBlobStorage interface for interacting with blob storage service

---
+++ InvoiceProcessor.API/Applications/Interfaces/IBlobStorage.cs
@@ -0,0 +1,8 @@
+public interface IBlobStorage
+{
+    Task<string> UploadAsync(Stream fileStream, string fileName);
+    Task<Stream> DownloadAsync(string fileName);
+    Task DeleteAsync(string fileName);
+}
+// This interface defines methods for interacting with a blob storage service.
+// It includes methods for uploading, downloading, and deleting files.
