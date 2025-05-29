43ddbe2ecd14ff93c8a88b8530021a2a364745ab
datpham0412 <tiendat041202@gmail.com>
2025-05-30 01:03:20 +1000

    feat: define IInvoiceRepository

---
+++ InvoiceProcessor.API/Applications/Interfaces/IInvoiceRepository.cs
@@ -0,0 +1,13 @@
+using InvoiceProcessor.Domain.Entities;
+public interface IInvoiceRepository
+{
+    Task<Invoice?> GetByIdAsync(Guid id);
+    Task<List<Invoice>> GetAllAsync();
+    Task AddAsync(Invoice invoice);
+    Task UpdateAsync(Invoice invoice);
+    Task DeleteAsync(Guid id);
+    Task SaveChangesAsync();
+}
+// This interface defines the contract for a repository that manages Invoice entities.
+// It includes methods for retrieving, adding, updating, and deleting invoices,
+// as well as saving changes to the underlying data store. The methods are asynchronous,
