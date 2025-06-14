64257d0ce577ee44dd26701191c13f158ac4dd68
datpham0412 <tiendat041202@gmail.com>
2025-06-15 02:11:57 +1000

    fix(exceptionrecord): eliminate empty invoice creation by removing Invoice = new() default

--- InvoiceProcessor.API/Domain/Entities/ExceptionRecord.cs
+++ InvoiceProcessor.API/Domain/Entities/ExceptionRecord.cs
@@ -7,6 +7,6 @@ namespace InvoiceProcessor.API.Domain.Entities
         public Guid InvoiceId { get; set; }
         public string Reason { get; set; } = string.Empty;
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
-        public virtual Invoice Invoice { get; set; } = new();
+        public virtual Invoice? Invoice { get; set; }
     }
 }
