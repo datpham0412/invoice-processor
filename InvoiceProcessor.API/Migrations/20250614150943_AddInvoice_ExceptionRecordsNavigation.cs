ccf324242a2acb55cb45b91ff12e20b821bbbed9
datpham0412 <tiendat041202@gmail.com>
2025-06-15 01:32:37 +1000

    chore(db): add migration for Invoice-ExceptionRecord relationship

---
+++ InvoiceProcessor.API/Migrations/20250614150943_AddInvoice_ExceptionRecordsNavigation.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceProcessor.API.Migrations
+{
+    public partial class AddInvoice_ExceptionRecordsNavigation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
