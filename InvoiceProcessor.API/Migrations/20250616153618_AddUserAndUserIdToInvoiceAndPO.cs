40e278169dc0aab8e4a10056b79d8bed7a97db12
datpham0412 <tiendat041202@gmail.com>
2025-06-17 02:46:39 +1000

    Add EF Core migration to include UserId column in Invoice and PurchaseOrder tables

---
+++ InvoiceProcessor.API/Migrations/20250616153618_AddUserAndUserIdToInvoiceAndPO.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceProcessor.API.Migrations
+{
+    public partial class AddUserAndUserIdToInvoiceAndPO : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "PurchaseOrders",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Invoices",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Password", "UserName" },
+                values: new object[] { "datpham0412", "Dat041202", "tiendat041202@gmail.com" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Password", "UserName" },
+                values: new object[] { "userA", "passA", "userA@example.com" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseOrders_UserId",
+                table: "PurchaseOrders",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_UserId",
+                table: "Invoices",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_Users_UserId",
+                table: "Invoices",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PurchaseOrders_Users_UserId",
+                table: "PurchaseOrders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_Users_UserId",
+                table: "Invoices");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PurchaseOrders_Users_UserId",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PurchaseOrders_UserId",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_UserId",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Invoices");
+        }
+    }
+}
