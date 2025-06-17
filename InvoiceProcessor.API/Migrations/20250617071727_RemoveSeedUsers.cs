e74f7604173b0eeb5f4c7c47d6eaacc9e481c468
datpham0412 <tiendat041202@gmail.com>
2025-06-17 17:21:33 +1000

    EF Migrations: remove seed data and reset DB schema for clean auth start

---
+++ InvoiceProcessor.API/Migrations/20250617071727_RemoveSeedUsers.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceProcessor.API.Migrations
+{
+    public partial class RemoveSeedUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "datpham0412");
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "userA");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Password", "UserName" },
+                values: new object[] { "datpham0412", "Dat041202", "tiendat041202@gmail.com" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Password", "UserName" },
+                values: new object[] { "userA", "passA", "userA@example.com" });
+        }
+    }
+}
