199fb21ea3ce8bd303afc4172802fa44c0dfde99
datpham0412 <tiendat041202@gmail.com>
2025-06-17 17:21:31 +1000

    Add RegisterRequest and LoginRequest DTOs for Auth endpoints

---
+++ InvoiceProcessor.API/Application/Models/Auth/LoginRequest.cs
@@ -0,0 +1,2 @@
+namespace InvoiceProcessor.API.Application.Models.Auth;
+public record LoginRequest(string UserName, string Password);
---
+++ InvoiceProcessor.API/Application/Models/Auth/RegisterRequest.cs
@@ -0,0 +1,2 @@
+namespace InvoiceProcessor.API.Application.Models.Auth;
+public record RegisterRequest(string UserName, string Password);
