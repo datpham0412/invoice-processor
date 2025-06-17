38d70a2aada0a38d95826c2d8f24f467aede6e84
datpham0412 <tiendat041202@gmail.com>
2025-06-17 13:13:17 +1000

    Refactor IInvoiceRepository to enforce user-scoped access for GetAll and Delete methods

--- InvoiceProcessor.API/Application/Interfaces/IInvoiceRepository.cs
+++ InvoiceProcessor.API/Application/Interfaces/IInvoiceRepository.cs
@@ -2,11 +2,11 @@ using InvoiceProcessor.API.Domain.Entities;
 namespace InvoiceProcessor.API.Application.Interfaces;
 public interface IInvoiceRepository
 {
-    Task<Invoice?> GetByIdAsync(Guid id);
-    Task<List<Invoice>> GetAllAsync();
+    Task<Invoice?> GetByIdAsync(Guid id, string userId);
+    Task<List<Invoice>> GetAllAsync(string userId);
     Task AddAsync(Invoice invoice);
     Task UpdateAsync(Invoice invoice);
-    Task DeleteAsync(Guid id);
+    Task DeleteAsync(Guid id, string userId);
     Task SaveChangesAsync();
 }
 // This interface defines the contract for a repository that manages Invoice entities.
