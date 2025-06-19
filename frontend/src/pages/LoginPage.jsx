b158298ff254d64cc91ae80fc9a488ffc026c909
datpham0412 <tiendat041202@gmail.com>
2025-06-19 22:39:40 +1000

    feat: add button to navigate from login to signup page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -24,6 +24,10 @@ export default function LoginPage() {
       <input value={userName} onChange={e => setUserName(e.target.value)} placeholder="Username" required />
       <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" required />
       <button type="submit">Login</button>
+      <p>Don't have an account?</p>
+      <button type="button" onClick={() => navigate('/signup')}>
+        Go to Sign Up
+      </button>
     </form>
   );
 }
