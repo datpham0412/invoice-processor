bc2b34316734a39d7cf2c11be2865ed5b305219f
datpham0412 <tiendat041202@gmail.com>
2025-06-26 17:09:29 +1000

    Enhance to show toast when session expires

--- frontend/src/components/ui/sonner.jsx
+++ frontend/src/components/ui/sonner.jsx
@@ -9,20 +9,9 @@ const Toaster = (props) => {
       position="top-right"
       toastOptions={{
         style: {
-          backgroundColor: "#1f2937", // default fallback (gray-800)
-          color: "#ffffff",
-        },
-        success: {
-          style: {
-            backgroundColor: "#16a34a", // green-600
-            color: "#ffffff",
-          },
-        },
-        error: {
-          style: {
-            backgroundColor: "#dc2626", // red-600
-            color: "#ffffff",
-          },
+         "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
         },
       }}
     />
