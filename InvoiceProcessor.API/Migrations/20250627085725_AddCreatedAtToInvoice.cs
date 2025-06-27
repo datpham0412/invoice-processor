1547170ede7179982ee26d60237a066735d5d9b4
datpham0412 <tiendat041202@gmail.com>
2025-06-27 21:01:39 +1000

    chore(db): add migration for CreatedAt timestamp on Invoice

---
+++ InvoiceProcessor.API/Migrations/20250627085725_AddCreatedAtToInvoice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceProcessor.API.Migrations
+{
+    public partial class AddCreatedAtToInvoice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Invoices",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Invoices");
+        }
+    }
+}
