7256a8329a2fbe9c934a0cf1d8481a6da1d990d9
datpham0412 <tiendat041202@gmail.com>
2025-06-27 21:01:38 +1000

    chore(db): add migration for Filename column on Invoice

---
+++ InvoiceProcessor.API/Migrations/20250627084754_AddFilenameToInvoice.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceProcessor.API.Migrations
+{
+    public partial class AddFilenameToInvoice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Filename",
+                table: "Invoices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Filename",
+                table: "Invoices");
+        }
+    }
+}
