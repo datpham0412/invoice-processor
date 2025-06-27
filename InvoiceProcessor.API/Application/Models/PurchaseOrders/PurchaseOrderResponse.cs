8b08e9f96644d54a38ece35054b611383b66d558
datpham0412 <tiendat041202@gmail.com>
2025-06-28 00:28:47 +1000

    feat(dto): include Status in PurchaseOrderResponse

--- InvoiceProcessor.API/Application/Models/PurchaseOrders/PurchaseOrderResponse.cs
+++ InvoiceProcessor.API/Application/Models/PurchaseOrders/PurchaseOrderResponse.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using InvoiceProcessor.API.Domain.Enums;
 
 namespace InvoiceProcessor.API.Application.Models.PurchaseOrders{
     public class PurchaseOrderResponse{
@@ -9,6 +10,7 @@ namespace InvoiceProcessor.API.Application.Models.PurchaseOrders{
         public DateTime IssueDate { get; set; }
         public List<PurchaseOrderLineItemResponse> LineItems { get; set; } = new();
         public decimal TotalAmount { get; set; }
+        public InvoiceStatus Status { get; set; }
     }
     public class PurchaseOrderLineItemResponse{
         public string Description { get; set; } = string.Empty;
