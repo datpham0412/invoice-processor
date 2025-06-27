49f6b572ec0219ff9fa928e534fecb13f7af09b6
datpham0412 <tiendat041202@gmail.com>
2025-06-27 21:00:41 +1000

    feat(invoice): add Filename and CreatedAt properties to Invoice

--- InvoiceProcessor.API/Domain/Entities/Invoice.cs
+++ InvoiceProcessor.API/Domain/Entities/Invoice.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using InvoiceProcessor.API.Domain.Enums;
 namespace InvoiceProcessor.API.Domain.Entities
 {
@@ -6,16 +7,21 @@ namespace InvoiceProcessor.API.Domain.Entities
         public Guid Id { get; set; }
         public string InvoiceNumber { get; set; } = string.Empty;
         public string? PoNumber { get; set; }
-
         public string VendorName { get; set; } = string.Empty;
         public DateTime InvoiceDate { get; set; }
         public decimal TotalAmount { get; set; }
         public InvoiceStatus Status { get; set; }
         public List<LineItem> LineItems { get; set; } = new();
         public string BlobUrl {get; set; } = string.Empty;
+        public string Filename { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
         public List<ExceptionRecord> ExceptionRecords { get; set; } = new();
         public string UserId { get; set; } = default!;
         public AppUser User { get; set; } = default!;
         public decimal CalculateTotal() => LineItems.Sum(x => x.Amount);
+        public DateTime? ExtractionStartedAt { get; set; }
+        public DateTime? ExtractionCompletedAt { get; set; }
+        [NotMapped]
+        public TimeSpan? ExtractionDuration => ExtractionCompletedAt - ExtractionStartedAt;
     }
 }
