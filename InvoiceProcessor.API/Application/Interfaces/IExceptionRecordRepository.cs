23d73f5ce4b53fb44adf4688141dc1ec56eb6a54
datpham0412 <tiendat041202@gmail.com>
2025-06-29 00:34:15 +1000

    feat: update IExceptionRecordRepository to persist match results

--- InvoiceProcessor.API/Application/Interfaces/IExceptionRecordRepository.cs
+++ InvoiceProcessor.API/Application/Interfaces/IExceptionRecordRepository.cs
@@ -6,5 +6,6 @@ public interface IExceptionRecordRepository
     Task AddAsync(ExceptionRecord exception);
     Task<List<ExceptionRecord>> GetAllAsync();
     Task SaveChangesAsync();
+    Task<List<ExceptionRecord>> GetByInvoiceIdAsync(Guid invoiceId);
 }
 
