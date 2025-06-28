ebfd98fa10139e24631d24867b13a0c65b55032e
datpham0412 <tiendat041202@gmail.com>
2025-06-29 00:34:16 +1000

    feat: update ExceptionRecord entity with new timestamp and reason fields

--- InvoiceProcessor.API/Domain/Entities/ExceptionRecord.cs
+++ InvoiceProcessor.API/Domain/Entities/ExceptionRecord.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.Json.Serialization;
 namespace InvoiceProcessor.API.Domain.Entities
 {
     public class ExceptionRecord
@@ -7,6 +8,7 @@ namespace InvoiceProcessor.API.Domain.Entities
         public Guid InvoiceId { get; set; }
         public string Reason { get; set; } = string.Empty;
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        [JsonIgnore]
         public virtual Invoice? Invoice { get; set; }
     }
 }
